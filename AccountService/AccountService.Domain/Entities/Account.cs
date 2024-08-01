27edf0de478c033cd113ac21b7663503d22334fa
Giorgi Didebulidze <mr.didebulidze@gmail.com>
2024-08-01 23:48:27 +0400

    Fixed buisness logic for withdraw

--- AccountService/AccountService.Domain/Entities/Account.cs
+++ AccountService/AccountService.Domain/Entities/Account.cs
@@ -31,12 +31,12 @@
 
         public void Withdraw(decimal amount)
         {
-            if (amount <= 0)
+            if (amount >= 0)
             {
-                throw new ArgumentException("Withdrawal amount must be positive.");
+                throw new ArgumentException("Withdrawal amount must be Negative.");
             }
 
-            if (Balance - amount < 0)
+            if (Balance + amount < 0)
             {
                 throw new InvalidOperationException("Insufficient funds for this withdrawal.");
             }
